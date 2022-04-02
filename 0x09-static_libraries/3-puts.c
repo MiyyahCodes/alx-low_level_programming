d6ac6fbbb372a8bb2befca6c96e9e56d9f222f51
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-04-02 14:32:00 +0100

    Updated

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,14 @@
-#include "main.h"
-/**
-* _puts - print a string followed by a new line
-*@str: string to print
-*Return: Nothing
-*/
-void _puts(char *str)
-{
-	while (*str)
-        _putchar(*str++);
 
-      _putchar('\n');
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
