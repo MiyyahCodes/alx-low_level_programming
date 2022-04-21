910bfe8e653d5a4a91f4c69584338c5c989b6466
MiyyahCodes <adebimpeadefila64@gmail.com>
2022-04-21 13:52:55 -0700

    added files

---
+++ 4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *
+ * free_list - this frees a list
+ * @head: this is the head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
